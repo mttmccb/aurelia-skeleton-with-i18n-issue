a88d3537fd6786c01bb400a81d509fef2beecc3c
Matt McCabe <matt@koherent.uk>
2016-11-09 13:37:28 +0000

    Added basic i18n to skeleton-typescript

--- skeleton-typescript/src/main.ts
+++ skeleton-typescript/src/main.ts
@@ -1,5 +1,7 @@
 import 'bootstrap';
 import {Aurelia} from 'aurelia-framework';
+import * as Backend from 'i18next-xhr-backend';
+import * as LngDetector from 'i18next-browser-languagedetector';
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
@@ -11,6 +13,42 @@ export function configure(aurelia: Aurelia) {
 
   // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   // aurelia.use.plugin('aurelia-html-import-template-loader')
+  aurelia.use.plugin('aurelia-i18n', (instance: any) => {
+      // Fails on the line below
+      instance.i18next
+        .use(Backend)
+        .use(LngDetector);
 
+      return instance.setup({
+        backend: {
+          loadPath: '{{lng}}/{{ns}}',
+          parse: (data) => data,
+          ajax: loadLocales
+        },
+        detection: {
+          order: ['localStorage', 'cookie', 'navigator'],
+          lookupCookie: 'i18next',
+          lookupLocalStorage: 'i18nextLng',
+          caches: ['localStorage', 'cookie']
+        },
+        attributes: ['t', 'i18n'],
+        fallbackLng: 'en',
+        debug: false,
+        ns: ['translation']
+      });
+    });
+    
   aurelia.start().then(() => aurelia.setRoot());
+}
+
+function loadLocales(url, options, callback, data) {
+  try {
+    // include locale files as webpack chunks
+    let waitForLocale = require('../locales/' + url + '.json');
+    waitForLocale((locale) => {
+      callback(locale, {status: '200'});
+    });
+  } catch (e) {
+    callback(null, {status: '404'});
+  }
 }
